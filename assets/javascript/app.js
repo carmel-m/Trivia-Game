1b07cdcefda91186c1a614de4ac8d1a1ddf4781c
Carmel Meyer <cemeyer@elkera.com>
2019-10-24 18:55:07 -0400

    add questions, image assets, structure

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -4,19 +4,19 @@
 // store questions and answers in arrays
 // need a countdown timer
 // use .hide() and .show() methods (eg. when you click 'start game' the button will hide and qs show
-// get value from radio button
 
-// counter to keep track of score
+// VARIABLES
+// ==========================================================================
+
+// keeps track of answers/scores
 var correct = 0;
 var incorrect = 0;
 var unanswered = 0;
 
-// for tracking which question we're up to
+// keeps track of which number question is being displayed
 var counter = 0;
 
-// when it reaches questions.length, end game and show results/gif
-
-// questions array - use this or just put them in the html?
+// QUESTIONS ARRAY
 var questions = [
     {
         q: "In what year did Buffy first air?",
@@ -37,31 +37,83 @@ var questions = [
     {
         q: "What was Anya before she became human?",
         a: "Assassin Demon",
-        b: "Vengeance Demon",
+        b: "Mucus Demon",
         c: "Chaos Demon",
-        d: "Mucus Demon",
+        d: "Vengeance Demon",
+        e: "d"
+    },
+    {
+        q: "How many principals does Sunnydale High have throughout the series?",
+        a: "2",
+        b: "3",
+        c: "4",
+        d: "5",
+        e: "c"
+    },
+    {
+        q: "Who kills Jenny Calendar?",
+        a: "Willow",
+        b: "Angel",
+        c: "Oz",
+        d: "Spike",
         e: "b"
     },
+    {
+        q: "How old was Angel when he became a vampire?",
+        a: "26",
+        b: "19",
+        c: "29",
+        d: "22",
+        e: "a"
+    },
+    {
+        q: "In which episode does everyone lose their memory?",
+        a: "Hell's Bells",
+        b: "Out of My Mind",
+        c: "Tabula Rasa",
+        d: "Once More, With Feeling",
+        e: "c"
+    },
+    {
+        q: "What animal is Anya afraid of?",
+        a: "Bunnies",
+        b: "Goldfish",
+        c: "Kittens",
+        d: "Hamsters",
+        e: "a"
+    },
+    {
+        q: "Which sports team does Xander join at the end of season 2?",
+        a: "Tennis",
+        b: "Baseball",
+        c: "Football",
+        d: "Swimming",
+        e: "d"
+    },
+    {
+        q: "Where does Buffy work in season 6?",
+        a: "Chicken Castle",
+        b: "Doublemeat Palace",
+        c: "Super Duper Burger",
+        d: "Porky Pies",
+        e: "b"
+    }
 ];
 
-
-
-
+// function to display new questions/answer options on page
 var nextQ = function () {
-
     $("#question").html(questions[counter].q);
     $("#answer-a").html(questions[counter].a);
     $("#answer-b").html(questions[counter].b);
     $("#answer-c").html(questions[counter].c);
     $("#answer-d").html(questions[counter].d);
-
 }
 
-//when timer = 0, do .endGame()
+// FUNCTIONS
+// ==============================================================================
 
 
-
-// function will run when answer a is clicked on
+// function will run whenever option 'a' is clicked on
 $("#answer-a").on("click", function () {
     if (questions[counter].e === "a") {
         correct++;
@@ -72,10 +124,61 @@ $("#answer-a").on("click", function () {
         counter++;
         nextQ();
     }
- console.log(correct);
- console.log(incorrect);
-
+//  console.log(correct);
+//  console.log(incorrect);
 })
+
+// function will run whenever option 'b' is clicked on
+$("#answer-b").on("click", function () {
+    if (questions[counter].e === "b") {
+        correct++;
+        counter++;
+        nextQ();
+    } else {
+        incorrect++;
+        counter++;
+        nextQ();
+    }
+//  console.log(correct);
+//  console.log(incorrect);
+})
+
+// function will run whenever option 'c' is clicked on
+$("#answer-c").on("click", function () {
+    if (questions[counter].e === "c") {
+        correct++;
+        counter++;
+        nextQ();
+    } else {
+        incorrect++;
+        counter++;
+        nextQ();
+    }
+//  console.log(correct);
+//  console.log(incorrect);
+})
+
+// function will run whenever option 'd' is clicked on
+$("#answer-d").on("click", function () {
+    if (questions[counter].e === "d") {
+        correct++;
+        counter++;
+        nextQ();
+    } else {
+        incorrect++;
+        counter++;
+        nextQ();
+    }
+//  console.log(correct);
+//  console.log(incorrect);
+})
+
+// MAIN PROCESS
+// ==============================================================================
+
+//when timer = 0, do .endGame()
+
+// when counter = questions.length, end game and show results/gif
 
 nextQ();
 
